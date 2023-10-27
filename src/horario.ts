ec0e0fcb821578c6ca1c052b55bc1c2a0235b702
puchy22 <rubendltv22@gmail.com>
2023-10-27 13:49:23 +0200

    Definida las preferencia para la asignación de horas se solventa el problema de preferencia de asignación de horas definido en la #6

--- src/horario.ts
+++ src/horario.ts
@@ -3,6 +3,10 @@ import { Cuidador } from './Cuidador';
 
 class Horario{
 
+   readonly PORCENTAJE_DE_PREFERENCIA_ESTUDIANTE = 0.5;
+   readonly PORCENTAJE_DE_PREFERENCIA_TRABAJADOR = 0.75;
+   readonly PORCENTAJE_DE_PREFERENCIA_NO_TRABAJADOR = 0.85;
+
    private idPersonaAtendida: number;
 
    private cuidadoresDisponibles: Array<Cuidador>;
