a9ab77a7a63f8d0a846147c55910a969aea6895e
puchy22 <rubendltv22@gmail.com>
2023-10-30 20:28:09 +0100

    Cambio en los constructores se han añadido constructores a ambas clases con el uso de una sintaxis más breve y más correcta para TypeScript

--- lib/horario.ts
+++ lib/horario.ts
@@ -1,18 +1,15 @@
 
-import { PersonaCuidadora } from './persona_cuidadora';
+import { IntervaloHorario, PersonaCuidadora } from './persona_cuidadora';
 
 const LIMITE_HORAS_POR_DIA_PERSONA_JOVEN = 2;
 const LIMITE_HORAS_POR_DIA_PERSONA_ADULTA = 4;
 
 class Horario{
 
-   private idPersonaAtendida: number;
-   private personasCuidadorasDisponibles: Array<PersonaCuidadora>;
-
-   private getNumeroHorasLibres(personaCuidadora: PersonaCuidadora): number{
-      // TO DO
-      return 0;
-   }
+   constructor(
+      private idPersonaAtendida: number,
+      private personasCuidadorasDisponibles: Array<PersonaCuidadora>
+   ){}
 
    public elegirPersonaCuidadora(personasCuidadoras: Array<PersonaCuidadora>): PersonaCuidadora{
       // TO DO
--- lib/persona_cuidadora.ts
+++ lib/persona_cuidadora.ts
@@ -1,13 +1,15 @@
 
-interface IntervaloHorario {
+export interface IntervaloHorario {
    fechaInicio : Date;
    fechaFin : Date;
 }
 
 export class PersonaCuidadora{
    
-   readonly id: number;
-   readonly horarioDisponible: Array<IntervaloHorario>;
-   readonly esJoven : boolean;
+   constructor(
+      readonly id: number,
+      readonly horarioDisponible: Array<IntervaloHorario>,
+      readonly esJoven : boolean
+   ){}
 }
 
