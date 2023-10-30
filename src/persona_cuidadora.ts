a002bc4f5d33615e1db06468c4473b4a5c3619b3
puchy22 <rubendltv22@gmail.com>
2023-10-30 15:28:52 +0100

    Cambio ubicación del fichero de personas cuidadoras se ha transladado el fichero de la clas de personas cuidadoras a la carpeta de los ficheros fuentes

--- src/persona_cuidadora.ts
+++ src/persona_cuidadora.ts
@@ -9,11 +9,5 @@ export class PersonaCuidadora{
    readonly id: number;
    readonly horarioDisponible: Array<IntervaloHorario>;
    readonly esJoven : boolean;
-
-   constructor(id: number, horarioDisponible: Array<IntervaloHorario>, esJoven: boolean){
-      this.id = id;
-      this.horarioDisponible = horarioDisponible;
-      this.esJoven = esJoven;
-   }
 }
 
