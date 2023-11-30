bc6544e67d98e63e5c09fa06ee04dddc63481e11
Luzía <luciaglsn@correo.ugr.es>
2023-11-30 01:53:24 +0100

    Añade Drakefile #18
    Fin de la selección de toolchain closes #16

---
+++ drakefile.ts
@@ -0,0 +1,20 @@
+import { desc, run, task, sh} from "https://deno.land/x/drake@v1.6.0/mod.ts";
+
+// Tarea para verificar el código
+desc("Verificar código");
+task("verificarCodigo", [], function verificarCodigo() {
+    sh("deno check lib/horario.ts");
+});
+
+// Tarea para ejecutar la aplicación
+desc("Ejecutardeno la aplicación");
+task("ejecutarAplicacion", [], function ejecutarAplicacion() {
+    sh("deno run --allow-env --allow-net lib/horario.ts");
+});
+
+// Tarea por defecto
+desc("Tarea por defecto");
+task("default", ["verificarCodigo", "ejecutarAplicacion"]);
+
+// Ejecutar tareas al ejecutar el Drakefile
+run();
