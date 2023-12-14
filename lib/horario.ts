57353eca296d2c7880e7de5b409867440523144e
Luzía <luciaglsn@correo.ugr.es>
2023-12-14 23:37:17 +0100

    Implementa asignación horaria a cuidador #23
    Este commit implementa la lógica de negocio necesaria para abordar la HU1 #3. Se añade un algoritmo que asigna las horas de cuidado al cuidador que esté disponible en ese momento, priorizando más o menos según el tipo de persona que sea. En la función compararPorPrioridad se refleja el orden de la misma closes #23

--- lib/horario.ts
+++ lib/horario.ts
@@ -1,4 +1,5 @@
 import { Cuidador } from "./cuidador.ts";
+import { TipoPersona } from "./cuidador.ts";
 
 const HORAS_SEMANALES = 168;
 const LIMITE_HORAS_POR_SEMANA_EMPLEADO = 30, LIMITE_HORAS_POR_SEMANA_DESEMPLEADO = 40,
@@ -12,7 +13,41 @@ export class Horario{
    ){}
 
    public asignarTurnos(){
-      
+      for (let i = 0; i < HORAS_SEMANALES; i++){
+         for(let j = 0; j < this.cuidadoresDisponibles.length; j++){
+            this.cuidadoresDisponibles.sort(this.compararPorPrioridad);
+            let cuidador = this.cuidadoresDisponibles[i];
+            if (cuidador && cuidador.horasOcupadas && cuidador.horasOcupadas[i] === false) {
+               switch(true){
+                  case (cuidador.tipo == TipoPersona.DESEMPLEADO)
+                       && (cuidador.horasCuidado < LIMITE_HORAS_POR_SEMANA_DESEMPLEADO):
+                     this.turnos.set(i, cuidador);
+                     ++cuidador.horasCuidado;
+                     break;
+                  case (cuidador.tipo == TipoPersona.EMPLEADO) 
+                       && (cuidador.horasCuidado < LIMITE_HORAS_POR_SEMANA_EMPLEADO):
+                     this.turnos.set(i, cuidador);
+                     ++cuidador.horasCuidado;
+                     break;
+                  case (cuidador.tipo == TipoPersona.JOVEN) 
+                       && (cuidador.horasCuidado < LIMITE_HORAS_POR_SEMANA_PERSONA_JOVEN):
+                     this.turnos.set(i, cuidador);
+                     ++cuidador.horasCuidado;
+                     break;
+               }
+            }  
+         }
+      }
+   }
+
+   private compararPorPrioridad(a: Cuidador, b: Cuidador): number {
+      const prioridades = {
+         [TipoPersona.DESEMPLEADO]: 0,
+         [TipoPersona.EMPLEADO]: 1,
+         [TipoPersona.JOVEN]: 2
+      }
+ 
+      return prioridades[a.tipo] - prioridades[b.tipo];
    }
 
    public setHorasOcupadas(hora: number, duracion: number, cuidador: Cuidador){
